1d6cbf78fda4065c47e0fb2dcb372809223f491f
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-09-10 11:59:09 +0000

    Created a applications that adds two numbers

--- src/main/java/com/mycompany/gitup/Gitup.java
+++ src/main/java/com/mycompany/gitup/Gitup.java
@@ -11,6 +11,9 @@ package com.mycompany.gitup;
 public class Gitup {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a=55;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
     }
 }
